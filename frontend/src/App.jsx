8c4fd2b72cd75c97046a832d8cad221e20e8a488
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-29 13:46:31 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import ResumeBuilder from "./pages/ResumeBuilder.jsx";
+import ResumePreview from "./pages/ResumePreview.jsx";
+import PublicPortfolio from "./pages/PublicPortfolio.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/builder" element={<ResumeBuilder />} />
+        <Route path="/preview" element={<ResumePreview />} />
+        <Route path="/p/:slug" element={<PublicPortfolio />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}

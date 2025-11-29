c11bdaf16a32d467b8e3e93baee08fe64c913bf1
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-29 13:34:37 +0530

    Create Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import { useState, useContext } from "react";
+import client from "../api/axiosClient";
+import { AuthContext } from "../auth/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function Login() {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [err,setErr] = useState("");
+  const { login } = useContext(AuthContext);
+  const nav = useNavigate();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await client.post("/api/auth/login", { email, password });
+      login(res.data.user, res.data.token);
+      nav("/dashboard");
+    } catch (error) {
+      setErr(error.response?.data?.message || "Error");
+    }
+  };
+
+  return (
+    <div className="center-box">
+      <h2>Login</h2>
+      <form onSubmit={submit}>
+        <input placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} />
+        <input placeholder="Password" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
+        <button>Login</button>
+        <p className="error">{err}</p>
+      </form>
+      <p>New? <Link to="/signup">Signup</Link></p>
+    </div>
+  );
+}

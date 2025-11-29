283739922c799b90ac6d28eca99fa7dcc4944fbe
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-29 13:31:06 +0530

    Create axiosClient.js

---
+++ frontend/src/api/axiosClient.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const client = axios.create({ baseURL: API_BASE, headers: { "Content-Type": "application/json" }});
+
+client.interceptors.request.use(config => {
+  const token = localStorage.getItem("ai_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default client;

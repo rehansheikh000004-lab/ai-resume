fc48660f656d905ea422ac5741889f599f9538df
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-29 13:37:55 +0530

    Create PublicPortfolio.jsx

---
+++ frontend/src/pages/PublicPortfolio.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export default function PublicPortfolio() {
+  const { slug } = useParams();
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    async function load() {
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/resume/public/${slug}`);
+      setHtml(res.data);
+    }
+    load();
+  }, []);
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+}

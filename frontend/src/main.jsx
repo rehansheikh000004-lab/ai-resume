45ff6f8e6ff0c42efd104679bd77d654b48f2c4c
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-30 12:36:07 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./style.css";
 
-import { AuthProvider } from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
